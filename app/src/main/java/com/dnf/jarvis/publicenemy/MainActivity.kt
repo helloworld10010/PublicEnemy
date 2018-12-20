3fe264a6beae9e29d26f6db8cf11023e17a478b9
guo <guojam@outlook.com>
2018-12-20 09:50:23 +0800

    start

---
+++ app/src/main/java/com/dnf/jarvis/publicenemy/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dnf.jarvis.publicenemy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
